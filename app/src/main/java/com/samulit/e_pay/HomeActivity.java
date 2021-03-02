73b150c05191ea7c584c1d8194772fc2e753811d
DeveloperKits <69858580+DeveloperKits@users.noreply.github.com>
2021-03-02 15:02:23 +0600

    E pay

---
+++ app/src/main/java/com/samulit/e_pay/HomeActivity.java
@@ -0,0 +1,19 @@
+package com.samulit.e_pay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        bottomNavigationView = findViewById(R.id.Bottom_nav);
+    }
+}
---
+++ app/src/main/java/com/samulit/e_pay/MainActivity.java
@@ -0,0 +1,27 @@
+package com.samulit.e_pay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent goToMain = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(goToMain);
+                finish();
+                //progressBar.setVisibility(View.INVISIBLE);
+            }
+        },2000);
+    }
+}
