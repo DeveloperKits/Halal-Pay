2c3059f30a19221b921450c7a31e8e13f6f7fbe7
DeveloperKits <69858580+DeveloperKits@users.noreply.github.com>
2021-03-02 16:35:45 +0600

    change

--- app/src/main/java/com/samulit/e_pay/HomeActivity.java
+++ app/src/main/java/com/samulit/e_pay/HomeActivity.java
@@ -1,13 +1,23 @@
 package com.samulit.e_pay;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.samulit.e_pay.Fragment.HomeFragment;
+import com.samulit.e_pay.Fragment.ProfileFragment;
+import com.samulit.e_pay.Fragment.WalletFragment;
 
 public class HomeActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
+    private BottomNavigationView bottomNavigationView;
+    private HomeFragment homeFragment;
+    private WalletFragment walletFragment;
+    private ProfileFragment profileFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +25,38 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         bottomNavigationView = findViewById(R.id.Bottom_nav);
+        homeFragment = new HomeFragment();
+        walletFragment = new WalletFragment();
+        profileFragment = new ProfileFragment();
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home :
+                        setFragment(homeFragment);
+                        return true;
+
+                    case R.id.wallet :
+                        setFragment(walletFragment);
+                        return true;
+
+                    case R.id.profile :
+                        setFragment(profileFragment);
+                        return true;
+
+                    default:
+                        return false;
+                }
+            }
+        });
     }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.full_frame, fragment);
+        fragmentTransaction.commit();
+    }
+
+
 }
