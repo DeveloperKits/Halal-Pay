7f83d7c92129a360e59b18294009d62a327acd26
DeveloperKits <69858580+DeveloperKits@users.noreply.github.com>
2021-10-08 21:31:51 +0600

    update

--- app/src/main/java/com/samulit/halal_pay/Fragment/WalletFragment.java
+++ app/src/main/java/com/samulit/halal_pay/Fragment/WalletFragment.java
@@ -14,11 +14,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -36,10 +38,10 @@ import java.util.Map;
 
 
 public class WalletFragment extends Fragment {
-    private MaterialCardView Deposit, Withdraw;
+    private MaterialCardView Deposit, Withdraw, Add_Money;
     private ImageView Profile_Image;
-    private TextView UserName, Available_balance, TotalBalance, Withdraw_Balance, Week, Month, Year, InterestMoney,
-            InterestTest, SevenDays, FifteenDays, OneYear, FiveYears, OneMonth, Hint;
+    private TextView UserName, Available_balance, TotalBalance, Withdraw_Balance, InterestMoney,
+            InterestTest, SevenDays, FifteenDays, OneYear, ThreeYears, OneMonth, Hint;
 
     private DatabaseReference databaseReference, databaseReference2, databaseReference3, databaseReference4;
     private FirebaseUser firebaseUser;
@@ -47,6 +49,8 @@ public class WalletFragment extends Fragment {
     private String Transfer_Type, withdraw_Type, UserID, userName, userImage, usersCurrentBalance, userEmail,
             usersTotalBalance, userWithdrawBalance ,WeekMonthYear, bkash_mer, rocket_mer, nogod_mer, InterestType,
             interest_money, SevenDays_St, OneMonth_st, FifteenDays_st, OneYear_st, FiveYears_st, currentUserBalance, interest;
+
+    private View view;
 
 
     public WalletFragment() {
@@ -57,15 +61,13 @@ public class WalletFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_wallet, container, false);
+        view = inflater.inflate(R.layout.fragment_wallet, container, false);
 
         Deposit = view.findViewById(R.id.deposit);
         Withdraw = view.findViewById(R.id.withdraw_card);
+        Add_Money = view.findViewById(R.id.add_money);
         Profile_Image = view.findViewById(R.id.profileImage);
         UserName = view.findViewById(R.id.UserName);
-        Week = view.findViewById(R.id.week);
-        Month = view.findViewById(R.id.month);
-        Year = view.findViewById(R.id.year);
         TotalBalance = view.findViewById(R.id.Total_Balance);
         Available_balance = view.findViewById(R.id.available);
         Withdraw_Balance = view.findViewById(R.id.withdraw);
@@ -114,17 +116,6 @@ public class WalletFragment extends Fragment {
                             InterestTest.setVisibility(View.VISIBLE);
                         }
 
-
-                        if (!WeekMonthYear.equals(" ")){
-                            if (WeekMonthYear.equals("Week")) {
-                                Week.setBackground(getResources().getDrawable(R.drawable.background_text));
-                            }else if (WeekMonthYear.equals("Month")){
-                                Month.setBackground(getResources().getDrawable(R.drawable.background_text));
-                            }else {
-                                Year.setBackground(getResources().getDrawable(R.drawable.background_text));
-                            }
-                        }
-
                     }
                 }
 
@@ -135,19 +126,11 @@ public class WalletFragment extends Fragment {
             });
 
 
-            Withdraw.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    withdraw();
-                }
-            });
+            Withdraw.setOnClickListener(view -> withdraw());
 
-            Deposit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    deposit();
-                }
-            });
+            Deposit.setOnClickListener(view -> deposit());
+
+            Add_Money.setOnClickListener(view1 -> AddMoney());
 
             databaseReference2 = FirebaseDatabase.getInstance().getReference("InterestMoney");
             databaseReference2.addValueEventListener(new ValueEventListener() {
@@ -171,7 +154,6 @@ public class WalletFragment extends Fragment {
 
         return view;
     }
-
 
     @SuppressLint("NonConstantResourceId")
     private void withdraw() {
@@ -252,7 +234,9 @@ public class WalletFragment extends Fragment {
                 currentUserBalance = String.valueOf(Double.parseDouble(usersCurrentBalance) - Double.parseDouble(storeMoney));
                 databaseReference4.child("usesCurrentBalance").setValue(currentUserBalance);
 
-                Toast.makeText(getContext(), "Successfully done!", Toast.LENGTH_LONG).show();
+                Snackbar snackbar = Snackbar.make(view, R.string.text_label, Snackbar.LENGTH_LONG);
+                snackbar.setAction("OK", view -> snackbar.dismiss());
+                snackbar.show();
 
                 alertDialog.dismiss();
             }
@@ -262,8 +246,140 @@ public class WalletFragment extends Fragment {
     }
 
 
+    @SuppressLint("SetTextI18n")
+    private void AddMoney() {
+        final AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+        View mView = getLayoutInflater().inflate(R.layout.custom_dialog_box_deposit,null);
 
-    // Edit somethings
+        final TextView merchantNumber = mView.findViewById(R.id.merchantNum);
+        final TextView text = mView.findViewById(R.id.textView3);
+        final EditText paymentID = mView.findViewById(R.id.paymentID);
+        final EditText money = mView.findViewById(R.id.money);
+        final EditText number = mView.findViewById(R.id.paymentNumber);
+        Button btn_okay = (Button)mView.findViewById(R.id.done);
+        Button btn_cancel = (Button)mView.findViewById(R.id.cancel);
+        RadioGroup radioGroup = mView.findViewById(R.id.radioGroup);
+        RadioGroup radioGroup2 = mView.findViewById(R.id.radioGroup2);
+        RadioButton wallet = mView.findViewById(R.id.wallet);
+
+        radioGroup2.setVisibility(View.GONE);
+        text.setVisibility(View.GONE);
+        wallet.setVisibility(View.GONE);
+        number.setHint("Your Bkash Number");
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("MerchantNumber");
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+
+                    bkash_mer = snapshot.child("Bkash").getValue().toString();
+                    rocket_mer = snapshot.child("Rocket").getValue().toString();
+                    nogod_mer = snapshot.child("Nogod").getValue().toString();
+
+                    if (bkash_mer.isEmpty()){
+                        merchantNumber.setText("Merchant Number: Sorry not available");
+                    }else {
+                        merchantNumber.setText("Merchant Number: " + bkash_mer);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener((radioGroup1, i) -> {
+            switch(i) {
+                case R.id.bkash:
+                    Transfer_Type = "Bkash";
+                    if (bkash_mer.isEmpty()){
+                        merchantNumber.setText("Sorry not available");
+                    }else {
+                        merchantNumber.setText("Merchant Number: " + bkash_mer);
+                    }
+                    number.setHint("Your Bkash Number");
+                    break;
+
+                case R.id.rocket:
+                    Transfer_Type = "Rocket";
+                    if (rocket_mer.isEmpty()){
+                        merchantNumber.setText("Merchant Number: Sorry not available");
+                    }else {
+                        merchantNumber.setText("Merchant Number: " + rocket_mer);
+                    }
+                    number.setHint("Your Rocket Number");
+                    break;
+
+                case R.id.nogod:
+                    Transfer_Type = "Nogod";
+                    if (nogod_mer.isEmpty()){
+                        merchantNumber.setText("Merchant Number: Sorry not available");
+                    }else {
+                        merchantNumber.setText("Merchant Number: " + nogod_mer);
+                    }
+                    number.setHint("Your Nogod Number");
+                    break;
+            }
+        });
+
+        alert.setView(mView);
+        final AlertDialog alertDialog = alert.create();
+        alertDialog.setCanceledOnTouchOutside(false);
+
+        btn_cancel.setOnClickListener(v -> alertDialog.dismiss()); // OnClickListener replace with lambda
+        btn_okay.setOnClickListener(v -> {
+            String storePaymentID, StoreNumber, Money;
+            storePaymentID = paymentID.getText().toString();
+            StoreNumber = number.getText().toString();
+            Money = money.getText().toString();
+
+            if (storePaymentID.equals(" ") || storePaymentID.equals("")) {
+                paymentID.setError("Enter Yor Transfer ID");
+                paymentID.requestFocus();
+            } else if (StoreNumber.length() != 11) {
+                number.setError("Check your number");
+                number.requestFocus();
+            } else if (Money.equals(" ") || Money.equals("")) {
+                number.setError("Enter valid Amount");
+                number.requestFocus();
+            } else {
+
+                Calendar calFordDate = Calendar.getInstance();
+                SimpleDateFormat currentDate = new SimpleDateFormat("dd-MMMM-yyyy");
+                String saveCurrentDate = currentDate.format(calFordDate.getTime());
+
+                SimpleDateFormat currentTime = new SimpleDateFormat("hh:mm a");
+                String saveCurrentTime = currentTime.format(calFordDate.getTime());
+
+                Map reg = new HashMap();
+                reg.put("userName", userName);
+                reg.put("userEmail", userEmail);
+                reg.put("PaymentID", storePaymentID);
+                reg.put("depositNumber", StoreNumber);
+                reg.put("userUID", UserID);
+                reg.put("Date", saveCurrentDate);
+                reg.put("Time", saveCurrentTime);
+                reg.put("status", "pending...");
+                reg.put("DepositAmount", Money);
+                reg.put("depositType", Transfer_Type);
+                reg.put("InterestType", "Add Money");
+
+                databaseReference = FirebaseDatabase.getInstance().getReference("DepositRequest");
+                databaseReference.push().updateChildren(reg);
+
+                Snackbar snackbar = Snackbar.make(view, R.string.text_label, Snackbar.LENGTH_LONG);
+                snackbar.setAction("OK", view -> snackbar.dismiss());
+                snackbar.show();
+
+                alertDialog.dismiss();
+            }
+        });
+        alertDialog.show();
+    }
+
 
     @SuppressLint({"SetTextI18n", "NonConstantResourceId"})
     private void deposit() {
@@ -281,9 +397,13 @@ public class WalletFragment extends Fragment {
         RadioGroup radioGroup = mView.findViewById(R.id.radioGroup);
         RadioGroup radioGroup2 = mView.findViewById(R.id.radioGroup2);
         OneMonth = mView.findViewById(R.id.oneMonth);
+        OneYear = mView.findViewById(R.id.oneYear);
+        ThreeYears = mView.findViewById(R.id.ThreeYears);
 
         number.setHint("Your Bkash Number");
         OneMonth.setText("1 Month -------> " + OneMonth_st + "%");
+        OneYear.setText("1 Year -------> " + OneYear_st + "%");
+        ThreeYears.setText("3 Years -------> " + FiveYears_st + "%"); // The value of Five Years will serve as the value of three Years
         Transfer_Type = "Bkash";
         InterestType = "OneMonth";
 
@@ -358,6 +478,22 @@ public class WalletFragment extends Fragment {
             }
         });
 
+        radioGroup2.setOnCheckedChangeListener((radioGroup12, i) -> {
+            switch (i){
+                case R.id.oneMonth:
+                    InterestType = "OneMonth";
+                    break;
+
+                case R.id.oneYear:
+                    InterestType = "OneYear";
+                    break;
+
+                case R.id.ThreeYears:
+                    InterestType = "FiveYears";
+                    break;
+            }
+        });
+
         alert.setView(mView);
         final AlertDialog alertDialog = alert.create();
         alertDialog.setCanceledOnTouchOutside(false);
@@ -404,7 +540,9 @@ public class WalletFragment extends Fragment {
                     databaseReference = FirebaseDatabase.getInstance().getReference("DepositRequest");
                     databaseReference.push().updateChildren(reg);
 
-                    Toast.makeText(getContext(), "Successfully done! You will receive updates within 24 hours.", Toast.LENGTH_LONG).show();
+                    Snackbar snackbar = Snackbar.make(view, R.string.text_label, Snackbar.LENGTH_LONG);
+                    snackbar.setAction("OK", view -> snackbar.dismiss());
+                    snackbar.show();
 
                     alertDialog.dismiss();
                 }
@@ -449,7 +587,6 @@ public class WalletFragment extends Fragment {
                 }
 
             }
-
         }); // OnClickListener replace with lambda
 
         alertDialog.show();
