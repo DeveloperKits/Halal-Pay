43ddfa7c97b22e9e100484421692d5c1f26b31d1
DeveloperKits <69858580+DeveloperKits@users.noreply.github.com>
2021-11-03 20:08:09 +0600

    Game Home Activity added

---
+++ app/src/main/java/com/samulit/halal_pay/Game/GameHome.java
@@ -0,0 +1,16 @@
+package com.samulit.halal_pay.Game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.samulit.halal_pay.R;
+
+public class GameHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_home);
+    }
+}
